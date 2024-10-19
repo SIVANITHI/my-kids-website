124f9e9310ee2877c8be3e7905c00274ed58d6fd
SIVANITHI <105481662+SIVANITHI@users.noreply.github.com>
2024-10-19 14:21:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple interactivity for a fun message
+document.addEventListener('DOMContentLoaded', function() {
+    const welcomeMessage = "Welcome to Fun Kids World! Enjoy learning and playing!";
+    alert(welcomeMessage);
+});
